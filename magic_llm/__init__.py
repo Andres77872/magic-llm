81712f3b7623939afa3047f5dc92aae35e113226
andres77872 <andreslamosk124@hotmail.com>
2024-04-22 01:22:17 -0600

    Refactor chunk processing in EngineAnthropic
    The prepare_chunk, process_chunk methods were restructured to improve consistency and readability in the EngineAnthropic class. Also, the refactored methods now directly return ChatCompletionModel instances instead of raw JSON. This should improve type safety and make further processing of these chunks more straightforward.

--- magic_llm/__init__.py
+++ magic_llm/__init__.py
@@ -1,6 +1,6 @@
 from magic_llm.base import MagicLlmBase
 
-__version__ = '0.0.28'
+__version__ = '0.0.29'
 
 
 class MagicLLM(MagicLlmBase):
--- magic_llm/engine/engine_anthropic.py
+++ magic_llm/engine/engine_anthropic.py
@@ -1,12 +1,12 @@
 # https://docs.anthropic.com/claude/reference/messages-streaming
 import aiohttp
 import json
-import traceback
 import urllib.request
 import time
 
 from magic_llm.engine.base_chat import BaseChat
 from magic_llm.model import ModelChat, ModelChatResponse
+from magic_llm.model.ModelChatStream import ChatCompletionModel
 
 
 class EngineAnthropic(BaseChat):
@@ -16,127 +16,6 @@ class EngineAnthropic(BaseChat):
         super().__init__(**kwargs)
         self.base_url = 'https://api.anthropic.com/v1/messages'
         self.api_key = api_key
-
-    def prepare_data(self, chat: ModelChat, **kwargs):
-        headers = {
-            'Content-Type': 'application/json',
-            'x-api-key': self.api_key,
-            'anthropic-version': '2023-06-01',
-            'anthropic-beta': 'messages-2023-12-15',
-            'accept': 'application/json',
-            'user-agent': 'arz-magic-llm-engine',
-            **self.headers
-        }
-        preamble = chat.messages[0]['content'] if chat.messages[0]['role'] == 'system' else None
-        if preamble:
-            chat.messages.pop(0)
-
-        data = {
-            "model": self.model,
-            "messages": chat.messages,
-            **kwargs,
-            **self.kwargs
-        }
-        if preamble:
-            data['system'] = preamble
-
-        json_data = json.dumps(data).encode('utf-8')
-        return json_data, headers
-
-    def prepare_http_data(self, chat: ModelChat, **kwargs):
-        json_data, headers = self.prepare_data(chat, **kwargs)
-        # Create a request object with the URL, data, and headers.
-        return urllib.request.Request(self.base_url, data=json_data, headers=headers)
-
-    def generate(self, chat: ModelChat, **kwargs) -> ModelChatResponse:
-        with urllib.request.urlopen(self.prepare_http_data(chat, stream=False, **kwargs)) as response:
-            response_data = response.read()
-            encoding = response.info().get_content_charset('utf-8')
-            r = json.loads(response_data.decode(encoding))
-            return ModelChatResponse(**{
-                'content': r['content'][0]['text'],
-                'prompt_tokens': r['usage']['input_tokens'],
-                'completion_tokens': r['usage']['input_tokens'] + r['usage']['output_tokens'],
-                'total_tokens': r['usage']['output_tokens'],
-                'role': 'assistant'
-            })
-
-    def stream_generate(self, chat: ModelChat, **kwargs):
-        # Make the request and read the response.
-        with urllib.request.urlopen(self.prepare_http_data(chat, stream=True, **kwargs)) as response:
-            idx = None
-            usage = None
-            for chunk in response:
-                if chunk:
-                    evt = chunk.decode().split('data:')
-                    if len(evt) != 2:
-                        continue
-                    event = json.loads(evt[-1])
-                    chunk, idx, usage = self.prepare_chunk(event, idx, usage)
-                    if chunk:
-                        chunk = json.dumps(chunk)
-                        yield f'data: {chunk}\n'
-                        yield f'\n'
-            chunk = {
-                'id': idx,
-                'choices':
-                    [{
-                        'delta':
-                            {
-                                'content': '',
-                                'role': None
-                            },
-                        'finish_reason': None,
-                        'index': 0
-                    }],
-                'created': int(time.time()),
-                'model': self.model,
-                'usage': usage,
-                'object': 'chat.completion.chunk'
-            }
-            chunk = json.dumps(chunk)
-            yield f'data: {chunk}\n'
-            yield f'\n'
-            yield f'[DONE]'
-            yield f'\n'
-
-    async def async_stream_generate(self, chat: ModelChat, **kwargs):
-        json_data, headers = self.prepare_data(chat, stream=True, **kwargs)
-        async with aiohttp.ClientSession() as session:
-            async with session.post(self.base_url, data=json_data, headers=headers) as response:
-                idx = None
-                usage = None
-                async for chunk in response.content:
-                    if chunk:
-                        evt = chunk.decode().split('data:')
-                        if len(evt) != 2:
-                            continue
-                        event = json.loads(evt[-1])
-                        chunk, idx, usage = self.prepare_chunk(event, idx, usage)
-                        if chunk:
-                            yield f'data: {json.dumps(chunk)}\n\n'
-                chunk = {
-                    'id': idx,
-                    'choices':
-                        [{
-                            'delta':
-                                {
-                                    'content': '',
-                                    'role': None
-                                },
-                            'finish_reason': None,
-                            'index': 0
-                        }],
-                    'created': int(time.time()),
-                    'model': self.model,
-                    'usage': usage,
-                    'object': 'chat.completion.chunk'
-                }
-                chunk = json.dumps(chunk)
-                yield f'data: {chunk}\n'
-                yield f'\n'
-                yield f'[DONE]'
-                yield f'\n'
 
     def prepare_chunk(self, event: dict, idx, usage):
         chunk = None
@@ -171,3 +50,120 @@ class EngineAnthropic(BaseChat):
                 'object': 'chat.completion.chunk'
             }
         return chunk, idx, usage
+
+    def prepare_data(self, chat: ModelChat, **kwargs):
+        headers = {
+            'Content-Type': 'application/json',
+            'x-api-key': self.api_key,
+            'anthropic-version': '2023-06-01',
+            'anthropic-beta': 'messages-2023-12-15',
+            'accept': 'application/json',
+            'user-agent': 'arz-magic-llm-engine',
+            **self.headers
+        }
+        preamble = chat.messages[0]['content'] if chat.messages[0]['role'] == 'system' else None
+        if preamble:
+            chat.messages.pop(0)
+
+        data = {
+            "model": self.model,
+            "messages": chat.messages,
+            **kwargs,
+            **self.kwargs
+        }
+        if preamble:
+            data['system'] = preamble
+
+        json_data = json.dumps(data).encode('utf-8')
+        return json_data, headers
+
+    def prepare_http_data(self, chat: ModelChat, **kwargs):
+        json_data, headers = self.prepare_data(chat, **kwargs)
+        # Create a request object with the URL, data, and headers.
+        return urllib.request.Request(self.base_url, data=json_data, headers=headers)
+
+    def process_chunk(self, chunk: str, idx, usage):
+        chunk, idx, usage = self.prepare_chunk(json.loads(chunk), idx, usage)
+        return ChatCompletionModel(**chunk) if chunk else None, idx, usage
+
+    def generate(self, chat: ModelChat, **kwargs) -> ModelChatResponse:
+        with urllib.request.urlopen(self.prepare_http_data(chat, stream=False, **kwargs)) as response:
+            response_data = response.read()
+            encoding = response.info().get_content_charset('utf-8')
+            r = json.loads(response_data.decode(encoding))
+            return ModelChatResponse(**{
+                'content': r['content'][0]['text'],
+                'prompt_tokens': r['usage']['input_tokens'],
+                'completion_tokens': r['usage']['input_tokens'] + r['usage']['output_tokens'],
+                'total_tokens': r['usage']['output_tokens'],
+                'role': 'assistant'
+            })
+
+    def stream_generate(self, chat: ModelChat, **kwargs):
+        # Make the request and read the response.
+        with urllib.request.urlopen(self.prepare_http_data(chat, stream=True, **kwargs)) as response:
+            idx = None
+            usage = None
+            for chunk in response:
+                if chunk:
+                    evt = chunk.decode().split('data:')
+                    if len(evt) != 2:
+                        continue
+                    if c := self.process_chunk(evt[-1].strip(), idx, usage):
+                        idx = c[1]
+                        usage = c[2]
+                        if c[0]:
+                            yield c[0]
+            chunk = {
+                'id': idx,
+                'choices':
+                    [{
+                        'delta':
+                            {
+                                'content': '',
+                                'role': None
+                            },
+                        'finish_reason': None,
+                        'index': 0
+                    }],
+                'created': int(time.time()),
+                'model': self.model,
+                'usage': usage,
+                'object': 'chat.completion.chunk'
+            }
+            yield ChatCompletionModel(**chunk)
+
+    async def async_stream_generate(self, chat: ModelChat, **kwargs):
+        json_data, headers = self.prepare_data(chat, stream=True, **kwargs)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.base_url, data=json_data, headers=headers) as response:
+                idx = None
+                usage = None
+                async for chunk in response.content:
+                    if chunk:
+                        evt = chunk.decode().split('data:')
+                        if len(evt) != 2:
+                            continue
+                        if c := self.process_chunk(evt[-1].strip(), idx, usage):
+                            idx = c[1]
+                        usage = c[2]
+                        if c[0]:
+                            yield c[0]
+                chunk = {
+                    'id': idx,
+                    'choices':
+                        [{
+                            'delta':
+                                {
+                                    'content': '',
+                                    'role': None
+                                },
+                            'finish_reason': None,
+                            'index': 0
+                        }],
+                    'created': int(time.time()),
+                    'model': self.model,
+                    'usage': usage,
+                    'object': 'chat.completion.chunk'
+                }
+                yield ChatCompletionModel(**chunk)
