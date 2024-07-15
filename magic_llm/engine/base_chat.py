cb15695f953ea22a9d842304fccb1f29c9504cf0
andres <andres@arz.ai>
2024-07-14 19:15:33 -0600

    Update BaseChat class with callback and async_intercept_stream_generate
    The BaseChat class has been extended with new features for increased flexibility and functionality. The class now inherits from Python's ABC for abstract base classes. A callback function has been added to the class initialization, allowing for external functionality after processing. Additionally, a new method 'async_intercept_stream_generate' is added to wrap asynchronous functions, helping to monitor and react to the ongoing usage metrics.

--- magic_llm/engine/base_chat.py
+++ magic_llm/engine/base_chat.py
@@ -1,17 +1,35 @@
-from typing import Iterator, AsyncIterator
+from typing import Iterator, AsyncIterator, Callable, Awaitable
+import abc
+import functools
 
 from magic_llm.model import ModelChat
 from magic_llm.model.ModelChatStream import ChatCompletionModel
 
 
-class BaseChat:
+class BaseChat(abc.ABC):
     def __init__(self,
                  model: str,
                  headers: dict = None,
+                 callback: Callable = None,
                  **kwargs):
         self.kwargs = kwargs
         self.model = model
         self.headers = headers if headers else {}
+        self.callback = callback
+
+    @staticmethod
+    def async_intercept_stream_generate(func: Callable[..., Awaitable[AsyncIterator[ChatCompletionModel]]]):
+        @functools.wraps(func)
+        async def wrapper(self, chat: ModelChat, **kwargs) -> AsyncIterator[ChatCompletionModel]:
+            usage = None
+            async for item in func(self, chat, **kwargs):
+                if item.usage.total_tokens != 0:
+                    usage = item.usage
+                yield item
+            if self.callback:
+                self.callback(chat, usage)
+
+        return wrapper
 
     def generate(self, chat: ModelChat, **kwargs):
         pass
@@ -22,6 +40,7 @@ class BaseChat:
     def stream_generate(self, chat: ModelChat, **kwargs) -> Iterator[ChatCompletionModel]:
         pass
 
+    @abc.abstractmethod
     def async_stream_generate(self, chat: ModelChat, **kwargs) -> AsyncIterator[ChatCompletionModel]:
         pass
 
