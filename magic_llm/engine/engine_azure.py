f0e57fdeefd1976af325ab24808a7c3b9c059468
andres <andreslamosk124@hotmail.com>
2024-12-09 20:58:35 -0600

    Add Azure TTS integration in engine module
    Introduced the EngineAzure class for Azure Text-to-Speech functionality within the engine module. This includes asynchronous audio speech support and methods stubs for text generation features. The implemented functionality will utilize Azure's cognitive services for processing text input into speech.

---
+++ magic_llm/engine/engine_azure.py
@@ -0,0 +1,59 @@
+import aiohttp
+
+from magic_llm.engine.base_chat import BaseChat
+from magic_llm.model import ModelChat, ModelChatResponse
+from magic_llm.model.ModelAudio import AudioSpeechRequest
+
+
+class EngineAzure(BaseChat):
+    def __init__(self,
+                 speech_key: str,
+                 speech_region: str,
+                 **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.base_url = f"https://{speech_region}.tts.speech.microsoft.com/cognitiveservices/v1"
+        self.speech_key = speech_key
+
+    @BaseChat.async_intercept_generate
+    async def async_generate(self, chat: ModelChat, **kwargs) -> ModelChatResponse:
+        raise NotImplementedError
+
+    @BaseChat.sync_intercept_generate
+    def generate(self, chat: ModelChat, **kwargs) -> ModelChatResponse:
+        raise NotImplementedError
+
+    @BaseChat.sync_intercept_stream_generate
+    def stream_generate(self, chat: ModelChat, **kwargs):
+        raise NotImplementedError
+
+    @BaseChat.async_intercept_stream_generate
+    async def async_stream_generate(self, chat: ModelChat, **kwargs):
+        raise NotImplementedError
+
+    async def async_audio_speech(self, data: AudioSpeechRequest, **kwargs):
+        # Construct SSML (Speech Synthesis Markup Language) payload
+        lang = '-'.join(data.voice.split('-')[:2])
+        ssml_template = f"""
+        <speak version='1.0' xml:lang='{lang}'>
+            <voice xml:lang='{lang}' name='{data.voice}'>
+                {data.input}
+            </voice>
+        </speak>
+        """
+
+        # Headers required by the Microsoft Speech API
+        headers = {
+            "Ocp-Apim-Subscription-Key": self.speech_key,
+            "Content-Type": "application/ssml+xml",
+            "X-Microsoft-OutputFormat": "audio-16khz-128kbitrate-mono-mp3",
+            "User-Agent": "magic-audio https://arz.ai",
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.base_url,
+                                    headers=headers,
+                                    data=ssml_template.strip()) as response:
+                response.raise_for_status()
+                # Return the raw binary content of the response
+                audio_content = await response.read()
+                return audio_content
