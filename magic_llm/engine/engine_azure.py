c6416869b86eaa962d185a186c11310941ce2a42
andres <andreslamosk124@hotmail.com>
2024-12-11 14:48:17 -0600

    Add audio transcription support to Azure engine
    Introduced `async_audio_transcriptions` method to handle audio-to-text features using the Microsoft Speech API. Switched to `AsyncHttpClient` for HTTP operations and added necessary endpoint and headers for transcription requests.

--- magic_llm/engine/engine_azure.py
+++ magic_llm/engine/engine_azure.py
@@ -1,7 +1,7 @@
 from magic_llm.engine.base_chat import BaseChat
 from magic_llm.model import ModelChat, ModelChatResponse
-from magic_llm.model.ModelAudio import AudioSpeechRequest
-from magic_llm.util.http import async_http_post_raw_binary
+from magic_llm.model.ModelAudio import AudioSpeechRequest, AudioTranscriptionsRequest
+from magic_llm.util.http import AsyncHttpClient
 
 
 class EngineAzure(BaseChat):
@@ -11,6 +11,7 @@ class EngineAzure(BaseChat):
                  **kwargs) -> None:
         super().__init__(**kwargs)
         self.base_url = f"https://{speech_region}.tts.speech.microsoft.com/cognitiveservices/v1"
+        self.base_transcription_url = f"https://{speech_region}.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language="
         self.speech_key = speech_key
 
     @BaseChat.async_intercept_generate
@@ -47,7 +48,25 @@ class EngineAzure(BaseChat):
             "X-Microsoft-OutputFormat": "audio-16khz-128kbitrate-mono-mp3",
             "User-Agent": "magic-audio https://arz.ai",
         }
+        async with AsyncHttpClient() as client:
+            response = await client.post_raw_binary(url=self.base_url,
+                                                    headers=headers,
+                                                    data=ssml_template.strip())
+            return response
 
-        return await async_http_post_raw_binary(url=self.base_url,
-                                                headers=headers,
-                                                data=ssml_template.strip())
+    async def async_audio_transcriptions(self, data: AudioTranscriptionsRequest, **kwargs):
+        # TODO FIX ERROR FOR NON WAV FILES
+        # Headers as required by the Microsoft Speech API
+        headers = {
+            "Ocp-Apim-Subscription-Key": self.speech_key,
+            "Content-Type": "audio/wav",
+            "User-Agent": "magic-transcriptions https://arz.ai",
+        }
+
+        async with AsyncHttpClient() as client:
+            response = await client.post_json(
+                url=self.base_transcription_url + data.language,
+                headers=headers,
+                data=data.file)
+
+            return response
